2ab3cf744b91c9e4822dba95a7b3859a3c25dcab
Jonny Heggheim <hegjon@gmail.com>
2020-08-10 15:33:23 +0200

    Use qDebug for test.cpp

--- src/bitcoin/test.cpp
+++ src/bitcoin/test.cpp
@@ -9,19 +9,18 @@
 namespace bitcoin {
     bool TestBase58(bool silent, bool throws)
     {
-        using Print = Debug;
         std::vector<unsigned char> result;
         const char *anAddress = "1C3SoftYBC2bbDzCadZxDrfbnobEXLBLQZ";
         if (!bitcoin::DecodeBase58Check(anAddress, result)) {
             static constexpr auto err = "Base58 decode check fail!";
             if (throws) throw InternalError(err);
-            if (!silent) Print() << err;
+            if (!silent) qDebug() << err;
             return false;
         }
         QByteArray ba;
         ba.insert(0, reinterpret_cast<char *>(&result[0]), int(result.size()));
         auto hexDecoded = ba.toHex();
-        if (!silent) Print() << anAddress << "  ->  " << hexDecoded << "  (decoded)";
+        if (!silent) qDebug() << anAddress << "  ->  " << hexDecoded << "  (decoded)";
         ba = QByteArray::fromHex("00791fc195e712c142df4c4e14fd4ec5b302733832");
         result.resize(size_t(ba.length()));
         memcpy(&result[0], ba.constData(), size_t(ba.length()));
@@ -30,12 +29,12 @@ namespace bitcoin {
         if (!bitcoin::DecodeBase58Check(str, result2) || result2 != result) {
             static constexpr auto err = "Base58 Decode -> Encode results differ! Fail!";
             if (throws) throw InternalError(err);
-            if (!silent) Print() << err;
+            if (!silent) qDebug() << err;
             return false;
         }
-        if (!silent) Print() << ba.toHex() << "  ->  " << str << "  (encoded)";
+        if (!silent) qDebug() << ba.toHex() << "  ->  " << str.c_str() << "  (encoded)";
         bool ret = ba.toHex() == hexDecoded && str == std::string(anAddress);
-        if (!silent) Print() << (ret ? "Compare ok, success." : "Values differ -- ERROR!!");
+        if (!silent) qDebug() << (ret ? "Compare ok, success." : "Values differ -- ERROR!!");
         return ret;
     }
 }
